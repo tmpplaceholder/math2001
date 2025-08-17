1fd51a1557b669d1e61d8cc3860b9d58cba91015
placeholder <b5wcbqhembyd@proton.me>
2025-08-17 19:19:50 +0000

    tmp

--- Math2001/04_Proofs_with_Structure_II/01_Forall_Implies.lean
+++ Math2001/04_Proofs_with_Structure_II/01_Forall_Implies.lean
@@ -113,20 +113,75 @@ example : ¬ Prime 6 := by
 /-! # Exercises -/
 
 
+/-
 example {a : ℚ} (h : ∀ b : ℚ, a ≥ -3 + 4 * b - b ^ 2) : a ≥ 1 :=
-  sorry
+  calc
+    a ≥ -3 + 4 * 2 - 2 ^ 2 := by apply h
+    _ = 1 := by numbers
+-/
+
+example {a : ℚ} (h : ∀ b : ℚ, a ≥ -3 + 4 * b - b ^ 2) : a ≥ 1 :=
+  by
+  have h1 : a ≥ -3 + 4 * 2 - 2 ^ 2 := by apply h
+  addarith [h1]
+
+/-
+example {n : ℤ} (hn : ∀ m, 1 ≤ m → m ≤ 5 → m ∣ n) : 15 ∣ n := by
+  have h1 : 3 ∣ n
+  · apply hn
+    · numbers
+    · numbers
+  · have h2 : 5 ∣ n
+    · apply hn
+      · numbers
+      · numbers
+    · obtain ⟨a, h3⟩ := h1
+      obtain ⟨b, h4⟩ := h2
+      have h5 : n = 6 * n - 5 * n := by ring
+      have h6 : 6 * n - 5 * n = 6 * (5 * b) - 5 * n := by rw [h4]
+      rw [h5, h6, h3]
+      use 2 * b - a
+      ring
+-/
 
 example {n : ℤ} (hn : ∀ m, 1 ≤ m → m ≤ 5 → m ∣ n) : 15 ∣ n := by
-  sorry
+  have h1 : 1 ≤ (3:ℤ) → (3:ℤ) ≤ 5 → 3 ∣ n := by apply hn
+  have h2 : 1 ≤ (5:ℤ) → (5:ℤ) ≤ 5 → 5 ∣ n := by apply hn
+  have h3 : 1 ≤ (3:ℤ) := by numbers
+  have h4 : (3:ℤ) ≤ 5 := by numbers
+  have h5 : 1 ≤ (5:ℤ) := by numbers
+  have h6 : (5:ℤ) ≤ 5 := by numbers
+  have h7 : 3 ∣ n := by apply h1 h3 h4
+  have h8 : 5 ∣ n := by apply h2 h5 h6
+  obtain ⟨a, ha⟩ := h7
+  obtain ⟨b, hb⟩ := h8
+  have h9 : n = 6 * n - 5 * n := by ring
+  have h10 : 6 * n - 5 * n = 6 * (5 * b) - 5 * n := by rw [hb]
+  rw [h9, h10, ha]
+  use 2 * b - a
+  ring
 
 example : ∃ n : ℕ, ∀ m : ℕ, n ≤ m := by
-  sorry
+  use 0
+  intro m
+  extra
 
 example : ∃ a : ℝ, ∀ b : ℝ, ∃ c : ℝ, a + b < c := by
-  sorry
+  use 0
+  intro b
+  use 1 + b
+  extra
 
 example : forall_sufficiently_large x : ℝ, x ^ 3 + 3 * x ≥ 7 * x ^ 2 + 12 := by
-  sorry
+  use 7
+  intro x h1
+  have h2 : x * x ^ 2 + 3 * x ≥ 7 * x ^ 2 + 3 * 7 := by rel [h1]
+  have h3 : x ^ 3 + 3 * x = x * x ^ 2 + 3 * x := by ring
+  rw [h3]
+  addarith [h2]
 
 example : ¬(Prime 45) := by
-  sorry
+  apply not_prime 5 9
+  · numbers
+  · numbers
+  · numbers
